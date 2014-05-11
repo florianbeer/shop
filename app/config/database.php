e2df23212f79ea5205814d6f701a57888f3ddf99
Florian Beer <fb@42dev.eu>
2014-05-12 01:24:59 +0200

    Secure DB config via ENV

--- app/config/database.php
+++ app/config/database.php
@@ -53,14 +53,14 @@ return array(
 		),
 
 		'mysql' => array(
-			'driver'    => 'mysql',
-			'host'      => 'localhost',
-			'database'  => 'zshop',
-			'username'  => 'root',
-			'password'  => '',
-			'charset'   => 'utf8',
-			'collation' => 'utf8_unicode_ci',
-			'prefix'    => '',
+      'driver'    => 'mysql',
+      'host'      => getenv('DB_HOST'),
+      'database'  => getenv('DB_NAME'),
+      'username'  => getenv('DB_USER'),
+      'password'  => getenv('DB_PASSWORD'),
+      'charset'   => 'utf8',
+      'collation' => 'utf8_unicode_ci',
+      'prefix'    => '',
 		),
 
 		'pgsql' => array(
