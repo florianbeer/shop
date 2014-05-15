8591a5ec8d787f99673c0ecb253853e314e55690
Florian Beer <fb@42dev.eu>
2014-05-15 19:09:14 +0200

    Revert to session based cart storage. Cache storage broke the item de- & increment actions.

--- app/config/moltincart.php
+++ app/config/moltincart.php
@@ -1,7 +1,7 @@
 <?php
 
 return array(
-    'storage' => 'cache', //session, cache
+    'storage' => 'session', //session, cache
 
     // Cache
     // Laravel documentation for more information
@@ -11,7 +11,7 @@ return array(
     // Identifier
     // With a requestcookie (choose for storage: cache, the session will be expired), the cart could be reloaded from a http request, example: the customer could save his cart link (email, hyperlink) and reopen this later.
     // If there is no request, the cookie will be loaded.
-    'identifier' => 'requestcookie', //cookie, requestcookie
+    'identifier' => 'cookie', //cookie, requestcookie
 
     //Request Cookie
     'requestid' => 'CartID', //http input request identifier, example: your customer/backoffice could reload the cart in your shop controller, /public/shop?CartID=871f0bc18ca76e68bf7c3adf8f9426ef
