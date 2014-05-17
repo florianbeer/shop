29808a30de2d9357b8809fee2091877f09f91d95
Florian Beer <fb@42dev.eu>
2014-05-17 23:41:22 +0200

    Nowrap in footer only for language flags

--- app/views/partials/_footer.blade.php
+++ app/views/partials/_footer.blade.php
@@ -5,14 +5,16 @@
         <p>&copy;{{ date('Y') }} <a href="http://42dev.eu">42dev</a> - {{ Lang::get('misc.footer-text') }}.</p>
       </div>
       <div class="col-sm-6">
-        <div class="pull-right nowrap">
+        <div class="pull-right">
           <a href="#">Imprint</a> |
           <a href="#">Privacy policy</a> |
           <a href="#">Terms of service</a> |
           <a href="/contact">Contact us</a> |
+          <span class="nowrap">
           @foreach(Config::get('app.languages') as $locale => $language)
             <a href="{{ URL::route('switchLanguage', $locale) }}" title="{{ Lang::get('misc.switch-to-language', ['lang' => $language]) }}"><div class="flag flag-{{ $locale }}"></div></a>
           @endforeach
+          </span>
         </div>
       </div>
     </div>
