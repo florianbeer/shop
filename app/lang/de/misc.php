95409a078be5eed50788a92ba08a226bcec28321
Florian Beer <fb@42dev.eu>
2014-05-28 17:57:13 +0200

    Language file updates for footer

--- app/lang/de/misc.php
+++ app/lang/de/misc.php
@@ -3,61 +3,63 @@
 return [
 
   'footer-text' => 'Alle Rechte vorbehalten',
-  
+
   'created' => 'erstellt',
-  
+
   'updated' => 'aktualisiert',
-  
+
   'not-found' => 'nicht gefunden',
-  
+
   'title' => 'Titel',
-  
+
   'delete' => 'L&ouml;schen',
-  
+
   'deleted' => 'gel&ouml;scht',
-  
+
   'description' => 'Beschreibung',
-  
+
   'price' => 'Preis',
-  
+
   'tax' => 'Steuer',
-  
+
   'send' => 'Absenden',
-  
+
   'search' => 'Suche',
-  
+
   'password-remind' => 'Passwort vergessen',
-  
+
   'email' => 'Email',
-  
+
   'password-reset' => 'Passwort zur&uuml;cksetzen',
-  
+
   'password' => 'Passwort',
-  
+
   'password-confirmation' => 'Passwort Best&auml;tigung',
-  
+
   'password-reset-message' => 'Passwort wurde erfolgreich geändert',
-  
+
   'name' => 'Name',
-  
+
   'items' => 'Artikel',
-  
+
   'created-at' => 'Erstellt',
-  
+
   'error-generic' => 'Ein Fehler ist aufgetreten',
-  
+
   'toggle-navigation' => 'Navigation aus- und einklappen',
-  
+
   'home' => 'Home',
-  
+
   'saved' => 'Gespeichert',
-  
+
   'print' => 'Drucken',
-  
+
   'language-select' => 'Language',
-  
+
   'no-found' => 'Seite nicht gefunden',
-  
-  'switch-to-language' => 'Sprache auf :lang &auml;ndern'
-  
+
+  'switch-to-language' => ':lang',
+
+  'language' => 'Sprache'
+
 ];
--- app/lang/en/misc.php
+++ app/lang/en/misc.php
@@ -3,61 +3,63 @@
 return [
 
   'footer-text' => 'All rights reserved',
-  
+
   'created' => 'created',
-  
+
   'updated' => 'updated',
-  
+
   'not-found' => 'not found',
-  
+
   'title' => 'Title',
-  
+
   'delete' => 'Delete',
-  
+
   'deleted' => 'deleted',
-  
+
   'description' => 'Description',
-  
+
   'price' => 'Price',
-  
+
   'tax' => 'Tax',
-  
+
   'send' => 'Submit',
-  
+
   'search' => 'Search',
-  
+
   'password-remind' => 'Password remind',
-  
+
   'email' => 'Email',
-  
+
   'password-reset' => 'Reset password',
-  
+
   'password' => 'Password',
-  
+
   'password-confirmation' => 'Pssword confirmation',
-  
+
   'password-reset-message' => 'Password changed successfully',
-  
+
   'name' => 'Name',
-  
+
   'items' => 'Items',
-  
+
   'created-at' => 'Created',
-  
+
   'error-generic' => 'An error occured',
-  
+
   'toggle-navigation' => 'Toggle navigation',
-  
+
   'home' => 'Home',
-  
+
   'saved' => 'Saved',
-  
+
   'print' => 'Print',
-  
+
   'language-select' => 'Language',
-  
+
   'not-found' => 'Not found',
-  
-  'switch-to-language' => 'Switch to :lang'
-  
+
+  'switch-to-language' => ':lang',
+
+  'language' => 'Language'
+
 ];
