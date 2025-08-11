44809fa3d405adbfccfea3dfba79220dec8cd1ae
Florian Beer <fb@42dev.eu>
2025-08-11 11:36:49 +0200

    Change pixel provider

--- app/database/seeds/ProductsTableSeeder.php
+++ app/database/seeds/ProductsTableSeeder.php
@@ -15,7 +15,8 @@ class ProductsTableSeeder extends Seeder {
         'description' => $faker->realText($maxNbChars = 400, $indexSize = 2),
         'price' => $faker->randomFloat($nbMaxDecimals = 2, $min = 1, $max = 50),
         'tax' => 20,
-        'image' => $faker->imageUrl($width = 480, $height = 640, 'food').$faker->word(),
+        'image' => 'https://picsum.photos/seed/' . $faker->word() . '/480/640',
+        #'image' => $faker->imageUrl($width = 480, $height = 640, 'food').$faker->word(),
       ]);
     }
     
